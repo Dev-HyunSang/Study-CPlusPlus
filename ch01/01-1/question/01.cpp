07896e29360fe55f4791a0b26ae59210cef426e6
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-03-02 23:22:35 +0900

    [UPDATE🚀] 01-1 문제 1: 오답으로 수정함.

--- ch01/01-1/question/01.cpp
+++ ch01/01-1/question/01.cpp
@@ -1,27 +1,15 @@
 #include <iostream>
 
 int main(void) {
-    int val1;
-    std::cout << "1번째 정수 입력: ";
-    std::cin >> val1;
+    int num = 0;
+    int input;
 
-    int val2;
-    std::cout << "2번쪠 정수 입력: ";
-    std::cin >> val2;
+    for (int i = 0; i < 5; i++) {
+        std::cout << i + 1 << "번쩨 정수 입력: ";
+        std::cin >> input;
+        num += input;
+    }
 
-    int val3;
-    std::cout << "3번쪠 정수 입력: ";
-    std::cin >> val3;
-
-    int val4;
-    std::cout << "4번쪠 정수 입력: ";
-    std::cin >> val4;
-
-    int val5;
-    std::cout << "5번쪠 정수 입력: ";
-    std::cin >> val5;
-
-    int result = val1 + val2 + val3 + val4 + val5;
-    std::cout << "합계: " << result << std::endl;
+    std::cout << "합계" << num << std::endl;
     return 0;
 }
